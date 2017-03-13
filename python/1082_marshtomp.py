c63c7e878a5ac1f41af68ecefdffcc060ed3df28
uniqueandys <uniqueandys@gmail.com>
2017-03-13 17:14:42 +0800

    add notes to marshtomp

--- python/1082_marshtomp.py
+++ python/1082_marshtomp.py
@@ -1,7 +1,12 @@
+'''
+遍历一趟，若以字母m(M)开头的子字符串与marshtomp不区分大小写相同，
+则记录下其位置，最后将相应子字符串用fjxmlhx替换。
+'''
 import sys
 
 def isSame(str1, str2):
     for i in xrange(len(str1)):
+        # 不区分大小写相同
         sub = ord(str1[i]) - ord(str2[i])
         if sub not in {-32, 0, 32}:
             return False
@@ -21,6 +26,7 @@ for line in sys.stdin:
         while i < len(line)-len(ori)+1:
             if line[i] == 'm' or line[i] == 'M':
                 if isSame(line[i:i+len(ori)], ori):
+                    # 记录下起始位置
                     l.append(i)
                     i += len(ori)
                 else:
@@ -33,6 +39,7 @@ for line in sys.stdin:
         else:
             repLine = ""
             for j in xrange(len(l)):
+                # 其余字符，包括开头，中间，结尾
                 if j == 0:
                     repLine += line[:l[j]]
                 else:
@@ -41,5 +48,3 @@ for line in sys.stdin:
                 if j == len(l)-1:
                     repLine += line[l[j]+len(ori):]
             print(repLine)
-
-        
